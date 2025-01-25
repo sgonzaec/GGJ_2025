8712d83fa649a9899a47560afc6c942251f46201
Andrés Felipe Ramírez Hernández <andres.ramirez@experimentality.co>
2025-01-24 23:18:30 -0500

    menu

--- Assets/Scripts/ServerManager.cs
+++ Assets/Scripts/ServerManager.cs
@@ -13,8 +13,6 @@ using Unity.Netcode;
             else
             {
                 StatusLabels();
-
-                //SubmitNewPosition();
             }
 
             GUILayout.EndArea();
@@ -37,22 +35,5 @@ using Unity.Netcode;
             GUILayout.Label("Mode: " + mode);
         }
 
-        /*static void SubmitNewPosition()
-        {
-            if (GUILayout.Button(NetworkManager.Singleton.IsServer ? "Move" : "Request Position Change"))
-            {
-                if (NetworkManager.Singleton.IsServer && !NetworkManager.Singleton.IsClient)
-                {
-                    foreach (ulong uid in NetworkManager.Singleton.ConnectedClientsIds)
-                        NetworkManager.Singleton.SpawnManager.GetPlayerNetworkObject(uid).GetComponent<HelloWorldPlayer>().Move();
-                }
-                else
-                {
-                    var playerObject = NetworkManager.Singleton.SpawnManager.GetLocalPlayerObject();
-                    var player = playerObject.GetComponent<HelloWorldPlayer>();
-                    player.Move();
-                }
-            }
-        }*/
     }
 
